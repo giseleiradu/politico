c9f778027b8abeefd679170f494cff82743a55b2
Gisele Iradukunda <giseleiradukunda@rocketmail.com>
2019-02-19 11:07:39 +0200

    add .env file [#164069470]

--- src/configuration/constants.js
+++ src/configuration/constants.js
@@ -1,4 +1,3 @@
 export default {
-  PORT: process.env.PORT || 3000,
   baseUrl: '/api/v1',
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import constants from './configuration/constants';
 
 // Starting our Express server and pass it the port to listen to.
 
-app.listen(constants.PORT, () => {
-  console.log(`server is running to port:${constants.PORT}`);
+app.listen(process.env.PORT, () => {
+  console.log(`server is running to port:${process.env.PORT}`);
 });
